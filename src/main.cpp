7eeaa145666c094a07eef743115cf3c4b29dda75
Alex Luiz <65964826+lexcibien@users.noreply.github.com>
2025-10-31 22:31:19 -0300

    adding cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,35 @@
-// Simple WiFi network scanner application
-// Released to the public domain in 2022 by Earle F. Philhower, III
 #include <WiFi.h>
+
+const char* macToString(uint8_t mac[6]);
+const char* encToString(uint8_t enc);
+void getNetworkList();
+void connectToInternet();
+
+String ssid = "Apartamento 502";
+String password = "";
+int status = WL_IDLE_STATUS;
 
 void setup() {
   Serial.begin(115200);
-}
-
-const char *macToString(uint8_t mac[6]) {
-  static char s[20];
-  sprintf(s, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  return s;
-}
-
-const char *encToString(uint8_t enc) {
-  switch (enc) {
-    case ENC_TYPE_NONE: return "NONE";
-    case ENC_TYPE_TKIP: return "WPA";
-    case ENC_TYPE_CCMP: return "WPA2";
-    case ENC_TYPE_AUTO: return "AUTO";
-  }
-  return "UNKN";
+  delay(10000);
+  connectToInternet();
 }
 
 void loop() {
-  delay(5000);
-  Serial.printf("Beginning scan at %lu\n", millis());
+  if(WiFi.status() == WL_CONNECTED) {
+    Serial.println("Você está conectado!");
+    delay(1000);
+  }
+}
+
+void getNetworkList() {
+  delay(2000);
+  Serial.printf("Iniciando o escaneamento em %lu\n", millis());
   auto cnt = WiFi.scanNetworks();
   if (!cnt) {
-    Serial.printf("No networks found\n");
+    Serial.printf("Nenhuma rede encontrada\n");
   } else {
-    Serial.printf("Found %d networks\n\n", cnt);
+    Serial.printf("Encontrado %d redes\n\n", cnt);
     Serial.printf("%32s %5s %17s %2s %4s\n", "SSID", "ENC", "BSSID        ", "CH", "RSSI");
     for (auto i = 0; i < cnt; i++) {
       uint8_t bssid[6];
@@ -37,6 +37,52 @@ void loop() {
       Serial.printf("%32s %5s %17s %2d %4ld\n", WiFi.SSID(i), encToString(WiFi.encryptionType(i)), macToString(bssid), WiFi.channel(i), WiFi.RSSI(i));
     }
   }
-  Serial.printf("\n--- Sleeping ---\n\n\n");
-  delay(5000);
+  delay(2000);
+}
+
+void connectToInternet() {
+  getNetworkList();
+  Serial.print("Escreva o nome da internet para conectar:");
+  while (Serial.available() == 0) {
+  }
+  ssid = Serial.readStringUntil('\n');
+  Serial.print("Agora a senha da internet:");
+  while (Serial.available() == 0) {
+
+  }
+  password = Serial.readStringUntil('\n');
+
+  Serial.printf("O nome do wifi é %s e sua senha do wifi é %s.\n", ssid.c_str(), password.c_str());
+
+  Serial.print("Conectando");
+  WiFi.begin(ssid.c_str(), password.c_str());
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    if (WiFi.status() == WL_CONNECT_FAILED) {
+      Serial.print("A conexão falhou tente mais tarde");
+      break;
+    }
+  }
+  Serial.println();
+
+  Serial.printf("Conectado, IP address: %s\n", WiFi.localIP().toString().c_str());
+}
+
+const char* macToString(uint8_t mac[6]) {
+  static char s[20];
+  sprintf(s, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  return s;
+}
+
+const char* encToString(uint8_t enc) {
+  switch (enc) {
+    case ENC_TYPE_NONE: return "NONE";
+    case ENC_TYPE_TKIP: return "WPA";
+    case ENC_TYPE_CCMP: return "WPA2";
+    case ENC_TYPE_AUTO: return "AUTO";
+  }
+  return "UNKN";
 }
