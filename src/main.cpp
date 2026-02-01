b918a450ddf92aeb33ff47da687e841c56235da7
Alex Luiz <65964826+lexcibien@users.noreply.github.com>
2026-02-01 14:59:39 -0300

    implement RTC

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <RtcDS1302.h>
 
 // Pinos módulos
 
@@ -16,17 +17,22 @@
 
 #define PIN_LDR 26
 
+const String ssid = "Apartamento 502";
+const String password = "cobalto01";
+
+DHT_Unified dht(PIN_DHT11_DATA, DHT11);
+
+ThreeWire myWire(PIN_RTC_DAT, PIN_RTC_CLK, PIN_RTC_RST); // IO, SCLK, CE
+RtcDS1302<ThreeWire> RTC(myWire);
+
 const char* macToString(const uint8_t mac[6]);
 const char* encToString(uint8_t enc);
 void getNetworkList();
 void connectToInternet();
 void readIncidentLight();
 void readTemperatureAndHumidity();
-
-const String ssid = "Apartamento 502";
-const String password = "cobalto01";
-
-DHT_Unified dht(PIN_DHT11_DATA, DHT11);
+void printDateTime(const RtcDateTime& dt);
+void performPrintDateTime();
 
 uint32_t delayMS;
 
@@ -37,6 +43,45 @@ void setup() {
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   delayMS = sensor.min_delay / 1000;
+
+  Serial.printf("Data compilado: %s\n", __DATE__);
+  Serial.printf("Horário compilado: %s\n", __TIME__);
+
+  RTC.Begin();
+
+  auto compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
+
+  if (!RTC.IsDateTimeValid()) {
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+
+    Serial.println("RTC lost confidence in the DateTime!");
+    RTC.SetDateTime(compiled);
+    Serial.println("Set new DateTime");
+  }
+
+  if (RTC.GetIsWriteProtected()) {
+    Serial.println("RTC was write protected, enabling writing now");
+    RTC.SetIsWriteProtected(false);
+  }
+
+  if (!RTC.GetIsRunning()) {
+    Serial.println("RTC was not actively running, starting now");
+    RTC.SetIsRunning(true);
+  }
+
+  RtcDateTime now = RTC.GetDateTime();
+  if (now < compiled) {
+    Serial.println("RTC is older than compile time!  (Updating DateTime)");
+    RTC.SetDateTime(compiled);
+  } else if (now > compiled) {
+    Serial.println("RTC is newer than compile time. (this is expected)");
+  } else if (now == compiled) {
+    Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+  }
 }
 
 void loop() {
@@ -45,6 +90,7 @@ void loop() {
     Serial.println("Você está conectado!");
   }
   if (millis() - lastTime >= delayMS) {
+    performPrintDateTime();
     readTemperatureAndHumidity();
     lastTime = millis();
   }
@@ -105,28 +151,57 @@ void connectToInternet() {
   Serial.printf("Conectado, IP address: %s\n", WiFi.localIP().toString().c_str());
 }
 
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt) {
+  char datestring[26];
+
+  snprintf_P(datestring,
+    countof(datestring),
+    PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+    dt.Month(),
+    dt.Day(),
+    dt.Year(),
+    dt.Hour(),
+    dt.Minute(),
+    dt.Second());
+  Serial.print(datestring);
+}
+
+void performPrintDateTime() {
+  RtcDateTime now = RTC.GetDateTime();
+
+  printDateTime(now);
+  Serial.println();
+
+  if (!now.IsValid()) {
+    // Common Causes:
+    //    1) the battery on the device is low or even missing and the power line was disconnected
+    Serial.println("RTC lost confidence in the DateTime!");
+  }
+}
+
 void readTemperatureAndHumidity() {
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     Serial.println(F("Error reading temperature!"));
   } else {
-    Serial.printf("Temperature: %f °C", event.temperature);
+    Serial.printf("Temperature: %.2f °C\n", event.temperature);
   }
 
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    Serial.printf("Humidity: %f \\%", event.relative_humidity);
+  } else {
+    Serial.printf("Humidity: %.2f %%\n", event.relative_humidity);
   }
 }
 
 void readIncidentLight() {
   auto adcRead = static_cast<uint16_t>(analogRead(PIN_LDR));
-  auto lux = static_cast<uint8_t>(map(adcRead, 100, 1023, 0, 10));
-  Serial.printf("A quantidade de luz no ambiente é: %c lm\n", lux);
+  unsigned long lux = map(adcRead, 100, 1023, 0, 100);
+  Serial.printf("A quantidade de luz no ambiente é: %lu lm\n", lux);
 }
 
 const char* macToString(const uint8_t mac[6]) {
